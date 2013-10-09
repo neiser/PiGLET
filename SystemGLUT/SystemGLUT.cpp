5adb8ff9892a9b9a1c776122e5a28b652d60aed3
Andreas Neiser <neiser@kph.uni-mainz.de>
2013-10-09 18:25:03 +0200

    It works on normal machines nearly

--- SystemGLUT/SystemGLUT.cpp
+++ SystemGLUT/SystemGLUT.cpp
@@ -1,7 +1,34 @@
 #include "SystemGLUT.h"
 
 #include <system.h>
+#include <iostream>
+
+using namespace std;
+
+
+static GLApp* currentInstance;
+
+static void drawCallback() {
+    currentInstance->Draw();
+    glFlush();
+    glFinish();
+    glutSwapBuffers();
+}
+
 
 void RunGL(GLApp &app) {
-
+    int iArgc = 0;
+    glutInit(&iArgc, NULL);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("PiGLPlot");
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    cout << "Test" << endl;
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    currentInstance = &app;
+    glutIdleFunc(drawCallback);
+    glutMainLoop();
 }
