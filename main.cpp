dce990442a7754d1f39378acad5064e5439a28e3
mwolfes <mwolfes@t-online.de>
2013-10-10 17:57:30 +0200

    horizontal tiling implemented

--- main.cpp
+++ main.cpp
@@ -12,8 +12,9 @@ int main()
     // enable the vertex array always, since this is always used
     // but the GL_TEXTURE_COORD_ARRAY must not be enabled globally (otherwise: segfault!)
     glEnableClientState(GL_VERTEX_ARRAY);
-//    PiGLPlotApp app;
+    //  PiGLPlotApp app;
     testApp app;
+    PiGLPlotApp app;
     RunGL(app);
 }
 
