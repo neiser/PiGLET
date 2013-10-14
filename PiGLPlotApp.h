43b517d725b49528d981b7e6d3dd4eb59f1dfdce
Oliver Steffen <steffen@kph.uni-mainz.de>
2013-10-14 17:09:58 +0200

    Remove non-functional PiGLPlotApp

--- PiGLPlotApp.h
+++ PiGLPlotApp.h
@@ -8,27 +8,6 @@
 #include "Window.h"
 
 
-class PiGLPlotApp: public GLApp {
-protected:
-    static const vec2_t square[4];
-    static const vec2_t square_tex[4];
-    PiGLPlot::BlockList graph;
-
-    float phase;
-
-    TextLabel text;
-
-    int frame;
-
-
-public:
-    PiGLPlotApp();
-
-    virtual void Init();
-    virtual void Draw();
-
-};
-
 class testApp: public GLApp{
 //private:
 
