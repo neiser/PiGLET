840ad8def26750bbdf118fd77abe4b1abfd7a353
Andreas Neiser <neiser@kph.uni-mainz.de>
2013-10-18 20:37:07 +0200

    Starting EPICS ctrl bugfix

--- scripts/caput.pl
+++ scripts/caput.pl
@@ -17,9 +17,9 @@ unless(defined $chan && $chan->is_connected) {
 }
 
 my $n = 0;
-my $flag = 0;
+my $flag = 1;
 while(1) {
-  $flag = !$flag if $n % 1000 == 0;
+  #$flag = !$flag if $n % 1000 == 0;
   my $val = 0.2*sin(2*3.14*$n/100)-0.5+$flag+0.1*rand();
   #printf("Putting %f\n", $val);
   $chan->put($val);
