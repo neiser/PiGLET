2cbc70b39cce015027aa8aa7c0de1f1c61c1b782
Andreas Neiser <andreas.neiser@gmail.com>
2013-10-19 11:37:43 +0200

    Bit more refactoring

--- Window.cpp
+++ Window.cpp
@@ -11,22 +11,13 @@
 
 using namespace std;
 
-
-
-
-
-
-
-int Window::callback_remove_window(const string &arg)
-{
-    return _owner->RemoveWindow(_name);    
-}
-
 Window::Window(WindowManager *owner, const string &name, const float xscale, const float yscale) : 
     _owner(owner),
     _name(name), 
     _x_pixels(xscale), 
     _y_pixels(yscale) {
+    
+    // windows can remove themselves
     ConfigManager::I().addCmd(name+"_Remove",BIND_MEM_CB(&Window::callback_remove_window,this));
 }
 
@@ -34,6 +25,12 @@ Window::~Window()
 {
     ConfigManager::I().removeCmd(_name+"_Remove");
 }
+
+int Window::callback_remove_window(const string &arg)
+{
+    return _owner->RemoveWindow(_name);    
+}
+
 
 std::ostream& operator<<( std::ostream& stream, const Window& win ) {
     stream << "[ " << win.XPixels() << " x " << win.YPixels() << " ]";
--- WindowManager.cpp
+++ WindowManager.cpp
@@ -8,10 +8,73 @@
 
 using namespace std;
 
-std::ostream& operator<<( std::ostream& stream, const WindowManager& wman ) {
-    stream << "Main Window Size: " << wman.SizeX() << " x " << wman.SizeY() << " ]";
-    return stream;
+WindowManager::WindowManager(const int dx, const int dy): _size_x(dx), _size_y(dy)
+{
+    // register the callbacks in the ConfigManager
+    ConfigManager::I().addCmd("RemoveAllWindows",BIND_MEM_CB(&WindowManager::callback_remove_all_windows,this));
+    ConfigManager::I().addCmd("AddPlotWindow",BIND_MEM_CB(&WindowManager::callback_add_plotwindow,this));
 }
+
+int WindowManager::AddWindow(Window *win)
+{
+    int ret = win->Init();
+    if(ret==0) {
+        _window_list.push_back(win);
+        align_windows();
+    }
+    else {
+        // delete the window again if 
+        // init was unsuccessful
+        delete win;
+    }
+    return ret;
+}
+
+int WindowManager::RemoveWindow(const size_t n){
+    if ( n >= NumWindows() ) return 1;
+    delete _window_list.at(n);
+    _window_list.erase(_window_list.begin() + n);
+    align_windows();
+    return 0;
+}
+
+int WindowManager::RemoveWindow(const string &name)
+{
+    for(size_t i=0; i<NumWindows(); i++) {
+        if(_window_list[i]->Name() == name) {
+            RemoveWindow(i);
+            return 0;
+        }        
+    }
+    return 1;
+}
+
+void WindowManager::Draw(){
+    
+    float dy = 2. / _rows.size();
+    float dx = 0;
+    
+    float wscaley = 1. / _rows.size();
+    float wscalex = 1.;
+    
+    int i_window = 0;
+    for ( size_t row = 0; row < _rows.size() ; ++row){
+        dx = 2. / _rows.at(row);
+        for ( int in_row = 0 ; in_row < _rows.at(row) ; ++in_row ){
+            wscalex = 1. / _rows.at(row);
+            glPushMatrix();
+            glTranslatef(-1 + (dx / 2) + (in_row * dx ),1 - (dy / 2. ) - (row * dy ),0.);
+            glScalef( wscalex , wscaley ,1);
+            _window_list.at(i_window)->Draw();
+            i_window++;
+            glPopMatrix();
+        }
+    }
+    
+    
+}
+
+
 
 int WindowManager::callback_remove_all_windows(const string &arg){
     int i;
@@ -74,70 +137,7 @@ void WindowManager::align_windows(){
     }
 }
 
-WindowManager::WindowManager(const int dx, const int dy): _size_x(dx), _size_y(dy)
-{
-    // register the callbacks in the ConfigManager
-    ConfigManager::I().addCmd("RemoveAllWindows",BIND_MEM_CB(&WindowManager::callback_remove_all_windows,this));
-    ConfigManager::I().addCmd("AddPlotWindow",BIND_MEM_CB(&WindowManager::callback_add_plotwindow,this));
-}
-
-int WindowManager::AddWindow(Window *win)
-{
-    int ret = win->Init();
-    if(ret==0) {
-        _window_list.push_back(win);
-        align_windows();
-    }
-    else {
-        // delete the window again if 
-        // init was unsuccessful
-        delete win;
-    }
-    return ret;
-}
-
-int WindowManager::RemoveWindow(const size_t n){
-    if ( n >= NumWindows() ) return 1;
-    delete _window_list.at(n);
-    _window_list.erase(_window_list.begin() + n);
-    align_windows();
-    return 0;
-}
-
-int WindowManager::RemoveWindow(const string &name)
-{
-    for(size_t i=0; i<NumWindows(); i++) {
-        if(_window_list[i]->Name() == name) {
-            RemoveWindow(i);
-            return 0;
-        }        
-    }
-    return 1;
-}
-
-
-
-void WindowManager::Draw(){
-    
-    float dy = 2. / _rows.size();
-    float dx = 0;
-    
-    float wscaley = 1. / _rows.size();
-    float wscalex = 1.;
-    
-    int i_window = 0;
-    for ( size_t row = 0; row < _rows.size() ; ++row){
-        dx = 2. / _rows.at(row);
-        for ( int in_row = 0 ; in_row < _rows.at(row) ; ++in_row ){
-            wscalex = 1. / _rows.at(row);
-            glPushMatrix();
-            glTranslatef(-1 + (dx / 2) + (in_row * dx ),1 - (dy / 2. ) - (row * dy ),0.);
-            glScalef( wscalex , wscaley ,1);
-            _window_list.at(i_window)->Draw();
-            i_window++;
-            glPopMatrix();
-        }
-    }
-    
-    
+std::ostream& operator<<( std::ostream& stream, const WindowManager& wman ) {
+    stream << "Main Window Size: " << wman.SizeX() << " x " << wman.SizeY() << " ]";
+    return stream;
 }
