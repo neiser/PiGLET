dd141394ac86d1e3e555dd1e3f5027b0e9445bb4
Andreas Neiser <neiser@kph.uni-mainz.de>
2013-10-22 06:31:15 +0200

    Bit better mirror script

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ROOTDIR="/" # dont forget trailing slash
 OPTIONS=(-avH --delete
     --exclude '/dev'
     --exclude '/proc'
@@ -14,20 +13,22 @@ OPTIONS=(-avH --delete
     --exclude '/var/log'
 )
 
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-01:/ &
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-02:/ &
+shopt -s dotglob
+
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-01:$1/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-02:/ &
 wait
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-03:/ &
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-04:/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-03:$1/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-04:$1/ &
 wait
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-05:/ &
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-06:/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-05:$1/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-06:$1/ &
 wait
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-07:/ &
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-08:/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-07:$1/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-08:$1/ &
 wait
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-09:/ &
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-10:/ &
-rsync "${OPTIONS[@]}" /* a2onlinedisplay-11:/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-09:$1/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-10:$1/ &
+rsync "${OPTIONS[@]}" $1/* a2onlinedisplay-11:$1/ &
 wait
 echo "DONE!"
